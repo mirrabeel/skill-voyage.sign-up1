3b11a72e98758308b2dded36195887385c69371b
mirrabeel <miranda.mukeria.1@iliauni.edu.ge>
2024-12-09 10:28:46 -0800

    Delete signup.js

--- signup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoadedDOMC' , function(){
-    const signupForm = document.querySelector ('section');
-    signupForm.style.opacity = 0;
-
-    setTimeout (() => {
-        signupForm.style.transition = 'opacity 10s ease-in-out';
-        signupForm.style.opacity = 1;
-    }, 1000);
-});
